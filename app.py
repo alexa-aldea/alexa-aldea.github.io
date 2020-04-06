dc1bc8d844d8a79717a889f4390da125291ccd49
Alexa <alexandra.aldea@hotmail.com>
2020-04-07 00:27:21 +0200

    update

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 labels_mar = ['Mar'+str(i) for i in range(12, 32)]
-labels_apr = ['Apr'+str(i) for i in range(1, 5)]
+labels_apr = ['Apr'+str(i) for i in range(1, 7)]
 
 day_labels = labels_mar + labels_apr + []
 
 no_deaths = {
-    'SWE': [1, 0,  1,  1,  4,  1,  2,  1,  5,  4,  1,  6, 13, 22, 15, 28,  0,  5, 36, 34, 59, 69, 50, 15],
-    'NOR': [1, 0,  2,  0,  0,  0,  3,  1,  0,  0,  0,  3,  2,  2,  0,  5,  4,  3,  6,  7,  5,  6,  9,  3],
-    'DNK': [0, 0,  1,  1,  2,  0,  0,  2,  3,  4,  0, 11,  8,  2,  7, 11, 13,  7,  5, 13, 14, 19, 16, 22],
-    'UK' : [0, 0, 10, 14, 20, 16, 33, 40, 33, 56, 48, 54, 87, 41,115,181,260,209,180,381,563,569,684,708],
+    'SWE': [1, 0,  1,  1,  4,  1,  2,  1,  5,  4,  1,  6, 13, 22, 15, 28,  0,  5, 36, 34, 59, 69, 50, 15, 28, 76],
+    'NOR': [1, 0,  2,  0,  0,  0,  3,  1,  0,  0,  0,  3,  2,  2,  0,  5,  4,  3,  6,  7,  5,  6,  9,  3,  9,  5],
+    'DNK': [0, 0,  1,  1,  2,  0,  0,  2,  3,  4,  0, 11,  8,  2,  7, 11, 13,  7,  5, 13, 14, 19, 16, 22, 18,  8],
+    'UK' : [0, 0, 10, 14, 20, 16, 33, 40, 33, 56, 48, 54, 87, 41,115,181,260,209,180,381,563,569,684,708,621,439],
 }
 
 population = {
@@ -28,8 +28,8 @@ for country in population.keys():
 
 @app.route('/')
 def line():
-    return render_template('line_chart.html', values=no_deaths_per1M, labels=day_labels)
+    return render_template('index.html', values=no_deaths_per1M, labels=day_labels)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(debug=True)
